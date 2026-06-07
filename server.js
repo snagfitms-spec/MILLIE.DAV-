13898ebf47dfef8c7a3cf9c10d7c9957a1e393a5
Mildred Dube <snagfitms@gmail.com>
2026-06-07 10:37:00 +0200

    Add middleware and test route in server.js
    Updated server.js to include middleware and a test route.

--- server.js
+++ server.js
@@ -4,23 +4,23 @@ const cors = require("cors");
 
 const app = express();
 
-// Middleware
+/
 app.use(cors());
 app.use(express.json());
 
-// Test route
+
 app.get("/", (req, res) => {
     res.send("Backend is running");
 });
 
-// MongoDB connection (we will add your string next)
+
 const MONGO_URI = "mongodb+srv://snagfitms_db_user:t6slZMwQCcsB2h84@booking1.eemnqgj.mongodb.net/?appName=Booking1;
 
 mongoose.connect(MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
 
-// Start server
+/
 const PORT = 3000;
 
 app.listen(PORT, () => {
